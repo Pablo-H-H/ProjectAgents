0ad5c815e3122594acaea58b59b08ad6ccfd59d1
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-08-19 16:00:39 -0600

    Started model

--- MultiAgentes/FireModel.py
+++ MultiAgentes/FireModel.py
@@ -1,1 +1,15 @@
 from mesa import Model
+from mesa.datacollection import DataCollector
+from mesa.space import SingleGrid
+
+from ReadLevel import ReadLevel
+
+class FireModel(Model):
+    def __init__(self,file):
+        super().__init__()
+        self.height = 6
+        self.width = 8
+        self.grid = SingleGrid(self.width,self.height,torus=False)
+        self.walls, self.points, self.smoke = ReadLevel(filename=file)
+
+file = "./TestLevel.txt"
