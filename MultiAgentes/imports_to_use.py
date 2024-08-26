7d9c1d965b02c9679df73234168a179bcd7e4ae6
Cristian Chávez Guía <chavezguiacristian@gmail.com>
2024-08-26 09:32:26 -0600

    add agent firefighters

---
+++ MultiAgentes/imports_to_use.py
@@ -0,0 +1,18 @@
+from mesa import Agent, Model
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
+
+# %matplotlib inline
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+plt.rcParams["animation.html"] = "jshtml"
+matplotlib.rcParams['animation.embed_limit'] = 2**128
+
+import numpy as np
+import pandas as pd
+
+import time
+import datetime
+import random
