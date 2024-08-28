d9e56737c725f2d6eafded5cb6bd342e84394d16
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-08-28 09:49:21 -0600

    Finished flashover function with BFS

--- MultiAgentes/FireBehaviours.py
+++ MultiAgentes/FireBehaviours.py
@@ -1,5 +1,7 @@
-import MultiAgentes.imports_to_use as imports_to_use
-from MultiAgentes.imports_to_use import *
+import imports_to_use as imports_to_use
+from imports_to_use import *
+
+
 
 def smokePlace(model):
     placement = [place for place in model.mapCoords if model.mapCoords <= 2]
@@ -23,45 +25,44 @@ def smokePlace(model):
 
 def shockWave(model,loc):
     # All possible directions for fire to travel in
-    directions = 4
-    directionsX = [0, -1, 0, 1]
-    directionsY = [1, 0, -1, 0]
-    
+    directions = 4    
+    dirH = [0, -1, 0, 1]
+    dirV = [1, 0, -1, 0]
 
     if model.smoke == 3:
         for i in range(directions):
             if model.walls[loc[0]][loc[1]][i] > 0 or model.walls[loc[0]][loc[1]][i] <= 2:
                 model.walls[loc[0]][loc[1]][i] += 1 # Damage or break walls around original explosion
                 directions -= 1
-                directionsX.pop(i)
-                directionsY.pop(i)
+                dirH.pop(i)
+                dirV.pop(i)
 
             elif model.walls[loc[0]][loc[1]][i] == 4:
                 model.walls[loc[0]][loc[1]][i] = 3  # Destroy a closed door
                 directions -= 1
-                directionsX.pop(i)
-                directionsY.pop(i)
+                dirH.pop(i)
+                dirV.pop(i)
             
             elif model.walls[loc[0]][loc[1]][i] == 5:
                 model.walls[loc[0]][loc[1]][i] = 3  # Destroy an open door but do not remove from explosion direction
             
         for i in range(directions):
-            if model.smoke[loc + (directionsX[i], directionsY[i])] == 2:
-                shockAdvance(model,loc,(directionsX[i], directionsY[i]))    # Continue shockwave if fire
+            if model.smoke[loc + (dirH[i], dirV[i])] == 2:
+                shockAdvance(model,loc,(dirH[i], dirV[i]))    # Continue shockwave if fire
             else:
-                model.smoke[loc + (directionsX[i], directionsY[i])] = 2
+                model.smoke[loc + (dirH[i], dirV[i])] = 2
 
 def shockAdvance(model,origin,dir): # This should be approx the same as above
     return 0
 
+# Using BFS on the smoke group we 
 def flashOver(model,loc):
-    # Probably best to use a simple BFS to target all the cells that are connected
+    dirH = [0, -1, 0, 1]
+    dirV = [1, 0, -1, 0]
     visited = [[False for x in range(model.width)] for y in range(model.height)]
     startX, startY = loc[0], loc[1]
     q = [(startX,startY)]
-    group = []
-    dirH = [1, 0, 0, -1]
-    dirV = [0, 1, -1, 0]
+    smokeGroup = []
 
     while q:
         x,y = q.pop(0)
@@ -69,3 +70,12 @@ def flashOver(model,loc):
             continue
 
         visited[x][y] = True
+        smokeGroup.append((x,y))
+        model.smokes[x][y] += 1
+
+        for i in range(4):
+            newX, newY = x + dirH[i], y + dirV[i]
+            if (model.walls[x][y][i] == 0 or model.walls[x][y][i] == 3 or model.walls[x][y][i] == 5) and model.smokes == 1:
+                if not visited[newX][newY]:
+                    q.append((newX,newY))
+            else: continue
