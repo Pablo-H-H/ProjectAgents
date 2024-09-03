937538e267fa93079a285c6ffc5ae97d200127b6
Maarten van t Hoff <mvanthoff28@gmail.com>
2024-09-03 11:43:22 -0600

    Fixed flashover

--- MultiAgentes/FireBehaviours2.py
+++ MultiAgentes/FireBehaviours2.py
@@ -44,14 +44,14 @@ def findNeighbor(model,x,y):
 def shockWave(model,x,y,nX,nY):
     print(f"Shockwave initiated at ({x}, {y})")
 
-    dirX, dirY = nX, nY
+    dirX, dirY = nX[:], nY[:]
     dir = len(nX)
 
     print(f"Wall in cell ({x},{y}) is {model.walls[y][x]}")
     # I need to do something here that can recognize which direction the wall is even if dir is not of length 4.
     if model.smoke[y][x] >= 2:
         for i in range(dir):
-            if (dirX[i] != None) and (dirY[i] != None):
+            if (dirX[i] is not None) and (dirY[i] is not None):
                 print(f"Checking direction {i} from ({x}, {y})")            
                 wallStatus = model.walls[y][x][i]
                 if wallStatus in [1,2]:
@@ -73,44 +73,44 @@ def shockWave(model,x,y,nX,nY):
                     print(f"Destroyed open door at ({x}, {y}) in direction {i}")
                     print(model.walls[y][x])
 
-                if dirX[i] != None and dirY[i] != None:
+                if dirX[i] is not None and dirY[i] is not None:
                     newX, newY = x + dirX[i], y + dirY[i]
-                else:
-                    newX, newY = x, y
             
-                if (0 <= newX < model.width) and (0 <= newY < model.height) and ((newX, newY) != (x, y)):
-                    if model.smoke[newY][newX] == 2:
-                        print(f"Propagating shockwave to ({newX}, {newY})")
-                        tempDirX, tempDirY = [None]*4, [None]*4
-                        tempDirX[i], tempDirY[i] = dirX[i], dirY[i]
-                        print(f"{tempDirX}, {tempDirY}")
-                        shockWave(model, newX, newY, tempDirX, tempDirY)     # Continue shockwave if fire
-                    elif model.smoke[newY][newX] in [0, 1]:
-                        model.smoke[newY][newX] = 2
-                        print(f"Set fire at ({newX}, {newY})")
-                    else:
-                        print(f"Out of bounds: ({newX},{newY})")
+                    if (0 <= newX < model.width) and (0 <= newY < model.height):
+                        if model.smoke[newY][newX] == 2:
+                            print(f"Propagating shockwave to ({newX}, {newY})")
+                            tempDirX, tempDirY = [None]*4, [None]*4
+                            tempDirX[i], tempDirY[i] = dirX[i], dirY[i]
+                            print(f"{tempDirX}, {tempDirY}")
+                            shockWave(model, newX, newY, tempDirX, tempDirY)     # Continue shockwave if fire
+                        elif model.smoke[newY][newX] in [0, 1]:
+                            model.smoke[newY][newX] = 2
+                            print(f"Set fire at ({newX}, {newY})")
+                            flashOver(model, newX, newY)
+                        else:
+                            print(f"Out of bounds: ({newX},{newY})")
 
 # Using BFS on the smoke group we 
 def flashOver(model,posX,posY):
+    print(f"Initialized flashover at cell ({posX},{posY})")
     visited = [[False for x in range(model.width)] for y in range(model.height)]
     startX, startY = posX,posY
     q = [(startX,startY)]
-    smokeGroup = []
-
     while q:
         x,y = q.pop(0)
         if visited[y][x]:
             continue
 
         visited[y][x] = True
-        smokeGroup.append((x,y))
-        if model.smoke[y][x] == 1 and (x,y) != (startX,startY):
-            model.smoke[y][x] += 1
+
+        if model.smoke[y][x] == 1:
+            print(f"Converted smoke at cell ({x},{y}) to fire")
+            model.smoke[y][x] = 2
 
         for i in range(4):
             newX, newY = x + dirH[i], y + dirV[i]
-            if (model.walls[y][x][i] == 0 or model.walls[y][x][i] == 3 or model.walls[y][x][i] == 5) and model.smoke[y][x] == 1:
-                if not visited[newY][newX]:
+            if (0 <= newX < model.width and 0 <= newY < model.height) and (model.walls[y][x][i] in [0, 3, 5]):
+                if model.smoke[y][x] == 1 and not visited[newY][newX]:
+                    print(f"Added cell ({newX},{newY}) to queue")
                     q.append((newX,newY))
             else: continue
